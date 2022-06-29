12bb357e6fb4f8cea179a19eb20a4a7fc6087da2
Dmitriy <dmitriych@wizardsdev.com>
2022-06-29 20:07:49 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { NotificationsService } from './notifications/notifications.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await NotificationsService.initApp();
-  await app.listen(3000, () => console.log('Server run on port: ', 3000));
+  await app.listen(process.env.PORT || 5000, () => console.log('Server run on port: ', 3000));
 }
 bootstrap();
